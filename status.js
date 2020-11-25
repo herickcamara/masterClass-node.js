e009849ec6d6b8e61e945de19c86bc00d22ae046
herick camara <herickcamara851@gmail.com>
2020-11-25 10:16:58 -0300

    end stats

---
+++ status.js
@@ -0,0 +1,21 @@
+const { clear } = require('console');
+const os = require('os');
+
+setInterval(()=>{
+    const {freemem, totalmem} = os;
+
+    const mem = parseInt(freemem() / 1024 /1024)
+    const total = parseInt(totalmem()/ 1024/ 1024)
+    const percent = parseInt((mem / total)*100) 
+   
+    const stats = {
+        free:`${mem} MB`,
+        total:`${total} MB`,
+        percent:`${percent}%`
+    }
+  
+    console.clear()
+    console.table(stats)
+
+},1000)
+
